e76465f450d9a79097a6bc63c8af6092ef0e1934
Tuan Nguyen <ntuan.work@gmail.com>
2023-08-15 18:19:41 +0700

    fix lint

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-vjzx7bb9.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-vjzx7bb9.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
